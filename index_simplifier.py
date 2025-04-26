28a7ffe9504abd55f7c8e89cb34d5656e73771db
abrierley0 <brierley.ajb@gmail.com>
2025-04-26 21:47:23 +0100

    Index simplifier

---
+++ index_simplifier.py
@@ -0,0 +1,57 @@
+import string
+
+"""
+PURPOSE: This script simplifies alegebraic expressions containing indices.
+
+AUTHOR: Mr Adam James Brierley
+
+DATE: 26th April 2025
+"""
+
+def main():
+
+    expression = input("Provide expression: ")
+
+    index_list, var_list, coeff_list, op_list = expression_reader(expression)
+
+    print(f"Index list: {index_list}")
+    print(f"Variable list: {var_list}")
+    print(f"Coefficient list: {coeff_list}")
+    print(f"Operations list: {op_list}")
+
+
+
+def expression_reader(expression):
+
+    counter = 0
+    index_list = []
+    var_list = []
+    coeff_list = []
+    op_list = []
+
+    for char in expression:
+
+        counter += 1
+
+        if char == "^":
+            index_list.append(expression[counter])
+
+        if char in string.ascii_letters:
+            var_list.append(char)
+            coeff_list.append(expression[counter-1])
+
+        if char == "/" or char == "*":
+            op_list.append(char)
+
+
+
+    return index_list, var_list, coeff_list, op_list
+
+
+
+
+
+
+
+
+main()
